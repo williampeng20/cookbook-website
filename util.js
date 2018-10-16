ebbea89b96d3e8e9523f14ec2a9d3101413ee9ce
William Peng <wpen@berkeley.edu>
2018-10-16 10:46:32 -0700

    temporarily finished with landing page visual changes

---
+++ util.js
@@ -0,0 +1,6 @@
+function fade_in_element(el){
+    var element = document.getElementById(el)
+    element.style.transition = "opacity 3.0s";
+    element.style['transition-timing-function'] = 'cubic-bezier(0.3, 0.3, 0.5, 0.8)';
+    element.style.opacity = 1;
+}
